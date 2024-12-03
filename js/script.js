faca6895b7e02fa5eeab85f9764018d88995f88c
carlosvillaar <carlosvillar004@gmail.com>
2024-12-03 17:46:28 +0100

    feat: add button

--- js/script.js
+++ js/script.js
@@ -5,9 +5,13 @@ for (let i = 0; i < 10; i++){
   generateMail()
 }
 
+const button = document.querySelector('.btn');
+
 function generateMail(){
   axios.get(enpoint)
-    .then(response => {document.getElementById('mail-ul').innerHTML += `<li class="text-center list-unstyled">${response.data.response}</li>`})
+    .then(response => {document.getElementById('mail-ul').innerHTML += `<li class="list-group-item list-group-item-success text-center list-unstyled py-2 my-1">${response.data.response}</li>`})
     .catch(error => {console.log(`Errore! `, error)})
 }
+
+
 
